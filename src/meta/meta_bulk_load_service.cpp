312ba36f9657597d0ff716b6bfa8f11692bb56fc
ninsmiracle <ninsmiracle@126.com>
2022-10-26 17:41:18 +0800

    debug 23

--- src/meta/meta_bulk_load_service.cpp
+++ src/meta/meta_bulk_load_service.cpp
@@ -1070,26 +1070,28 @@ void bulk_load_service::bulk_load_cu_flush(int32_t app_id){
     ddebug_f("temp debug  app_id({}) _partitions_total_downloaded_file_size size is ({})",
              app_id,
              _partitions_total_downloaded_file_size.size());
+
     for(auto iter : _partitions_total_downloaded_file_size){
         dsn::gpid current_gpid = iter.first;
         //values like {"appId@partitionID:[bulkloadCU]"}
         std::string bulk_load_cu_values = std::to_string(app_id)+"@"+std::to_string(current_gpid.get_partition_index())+":["+std::to_string( iter.second) +"]";
-        ddebug_f("temp debug2  app_id({}) now  is in partition{}",
+
+        ddebug_f("temp debug2  app_id({}) now  is in partition{} downloaded cu size is {}",
                  app_id,
-                 std::to_string(current_gpid.get_partition_index()));
+                 std::to_string(current_gpid.get_partition_index()),
+                 std::to_string( iter.second));
 
-
-        std::stringstream out;
-        rapidjson::OStreamWrapper wrapper(out);
-        dsn::json::JsonWriter writer(wrapper);
-        dsn::json::json_encode(writer, bulk_load_cu_values);
+//        std::stringstream out;
+//        rapidjson::OStreamWrapper wrapper(out);
+//        dsn::json::JsonWriter writer(wrapper);
+//        dsn::json::json_encode(writer, bulk_load_cu_values);
 
         int64_t timestamp = dsn_now_ms() / 1000;
         char buf[20];
         dsn::utils::time_ms_to_date_time((uint64_t)timestamp * 1000, buf, sizeof(buf));
         std::string timestamp_str = buf;
 
-        _bulk_load_cu_writer->set_result(timestamp_str, "bulkload_cu@", out.str());
+        _bulk_load_cu_writer->set_result(timestamp_str, "bulkload_cu@", bulk_load_cu_values);
     }
 
 }
